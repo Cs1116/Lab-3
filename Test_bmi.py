35df7c2adfd16d09cf95343c164faf9dca53e388
Chester <chester27.22@ichat.sp.edu.sg>
2023-10-30 20:32:15 +0800

    Change of files

---
+++ Test_bmi.py
@@ -0,0 +1,16 @@
+import Lab2.bmi
+
+def test_bmi_normal_weight():
+    test=0
+    w=Lab2.bmi.calculate_bmi(height=1.71,weight=55)
+    assert(test==w)
+
+def test_bmi_under_weight():
+    test = 1
+    w=Lab2.bmi.calculate_bmi(height=1.71,weight=50)
+    assert(test==w)
+
+def test_bmi_over_weight():
+    test = 1
+    w=Lab2.bmi.calculate_bmi(height=1.71,weight=100)
+    assert(test==w)
